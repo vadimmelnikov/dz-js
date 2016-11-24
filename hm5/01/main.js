391081d37915d26c0ec8363faa9aed85984e0b52
Vadim <vadic_m@mail.ru>
2016-11-24 14:01:55 +0300

    Update main.js

--- hm5/01/main.js
+++ hm5/01/main.js
@@ -1,30 +1,15 @@
 
 var selected = document.getElementById('list');
-var active = document.getElementsByClassName('active');
-var acc;
 
-
-selected.addEventListener('click', function (event) {
-	event.preventDefault();
-	var target = event.target;
-	while (target != this) {
-		if (target.tagName == 'A') {
-			console.log('клик по ссылке');
-			open(target);
-			return;
-		}
-		target = target.parentNode;
-	}
+selected.addEventListener('click', function(event) {
+    event.preventDefault();
+    var target = event.target;
+    if (target.classList.contains('accordion__title')) {
+        var active = document.querySelector('.active');
+        if (active) {
+            active.classList.remove('active');
+        }
+        target.classList.add('active');
+    }
 })
-
-
-
-function open(node) {
-
-	if (acc) {
-		acc.classList.remove('active');
-	}
-	acc = node;
-	acc.classList.add('active');
-}
 
